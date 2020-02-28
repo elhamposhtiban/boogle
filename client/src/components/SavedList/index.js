a90bfa12cf1ab992c681a4fc050a6997713c9a9b
elham poshtiban@gmail.com <55120923+elhamposhtiban@users.noreply.github.com>
2020-02-28 13:24:32 -0600

    refactoring

--- client/src/components/SavedList/index.js
+++ client/src/components/SavedList/index.js
@@ -24,26 +24,7 @@ const SavedResults = () => {
 
 
     return(
-        <div className="card p-3">
-            <h5 className="card-title">Saved Books</h5>
 
-            <ul className="list-group">
-            {savedBooks.map(item => (
-            <li className="list-group-item mb-2" key={item.id}>
-            <button className="btn btn-danger m-2 float-right" data-uniqe-id={item.id}>
-                    Remove Book
-            </button>
-                <h2>{item.title}</h2>
-                <h5>{item.author}</h5>
-                <div className="mt-2">
-                <p className="img-fluid float-right">{item.description}</p>
-                <img className="img-fluid float-left" src={item.image} />
-                </div>
-            </li>
-            ))}
-            </ul>
-    
-        </div>
     )
 }
 
