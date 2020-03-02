36c3c6ea1c6bf195a7f1e20397ba748de997baee
elham poshtiban@gmail.com <55120923+elhamposhtiban@users.noreply.github.com>
2020-03-01 21:22:38 -0600

    changes inorder to push to heroku

--- routes/route-index.js
+++ routes/route-index.js
@@ -6,13 +6,10 @@ const apiRoutes = require("./api");
 router.use("/api", apiRoutes);
 
 
-router.get('/hello-world', (req, res) => {
-  res.send('Hello world');
-})
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 // router.get("*", (req, res) => {
